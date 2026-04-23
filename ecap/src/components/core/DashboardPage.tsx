453b29a9fe9f12071e3946862f2c66722ea29aba
SaikiranIvaturi <ivaturisaikiran@gmail.com>
2026-04-23 09:48:12 +0530

    Remove ecap folder from tracking

--- ecap/src/components/core/DashboardPage.tsx
+++
@@ -1,9 +0,0 @@
-import UnifiedDashboard from "./UnifiedDashboard";
-
-interface DashboardPageProps {
-  onNavigate?: (page: string) => void;
-}
-
-export default function DashboardPage({ onNavigate }: DashboardPageProps) {
-  return <UnifiedDashboard onNavigate={onNavigate} variant="grid" />;
-}
